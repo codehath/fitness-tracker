db2d4d3823fa76b4f5054650934e344c419cf898
Ruby <rubyseresin@gmail.com>
2024-11-13 15:17:27 +0000

    feat(auth): add user deletion webhook handler

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,42 +2,46 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../../models");
 
-// Webhook endpoint
 router.post("/", async (req, res) => {
-  console.log("⚡ Webhook endpoint hit");
   try {
     const { type, data } = req.body;
 
-    if (type === "user.created") {
-      const { email_addresses, first_name, last_name } = data;
+    switch (type) {
+      case "user.created":
+        const { email_addresses, first_name, last_name } = data;
+        const existingUser = await User.findOne({
+          email: email_addresses[0].email_address,
+        });
 
-      // Check if user already exists
-      const existingUser = await User.findOne({
-        email: email_addresses[0].email_address,
-      });
+        if (existingUser) {
+          return res.status(200).send("User already exists");
+        }
 
-      if (existingUser) {
-        return res.status(200).send("User already exists");
-      }
+        const user = new User({
+          email: email_addresses[0].email_address,
+          name: `${first_name || ""} ${last_name || ""}`.trim(),
+          password: "clerk-auth",
+          clerkId: data.id,
+          gender: null,
+          bodyType: null,
+          fitnessGoals: null,
+          age: null,
+          weight: null,
+          height: null,
+        });
 
-      const user = new User({
-        email: email_addresses[0].email_address,
-        name: `${first_name || ""} ${last_name || ""}`.trim(),
-        password: "clerk-auth",
-        clerkId: data.id,
-        gender: null,
-        bodyType: null,
-        fitnessGoals: null,
-        age: null,
-        weight: null,
-        height: null,
-      });
+        await user.save();
+        break;
 
-      await user.save();
-      console.log("✅ User created in MongoDB:", user);
+      case "user.deleted":
+        const deletedUser = await User.findOneAndDelete({ clerkId: data.id });
+        break;
+
+      default:
+        console.log("Unhandled webhook event type:", type);
     }
 
-    res.status(200).send("Webhook received");
+    res.status(200).send("Webhook processed");
   } catch (error) {
     console.error("Error handling webhook:", error);
     res.status(500).send("Server error");
