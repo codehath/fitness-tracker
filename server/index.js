de14cb019b25dd4e06726d5740357c39038462c2
codehath <codingfarhath@gmail.com>
2024-11-19 01:00:28 +0000

    feat(exercise api): implemented exercise routes using external exerciseDB api

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const cors = require('cors');
 const workoutLogRoutes = require('./routes/workoutLogRoutes');
 const userRoutes = require('./routes/userRoutes');
 const workoutPlanRoutes = require('./routes/workoutPlanRoutes');
+const exerciseRoutes = require('./routes/exerciseRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -16,6 +18,7 @@ app.use(express.json());
 app.use('/api/', workoutLogRoutes);
 app.use('/api/', userRoutes);
 app.use('/api/', workoutPlanRoutes);
+app.use('/api/', exerciseRoutes);
 
 // Connect to MongoDB
 mongoose
